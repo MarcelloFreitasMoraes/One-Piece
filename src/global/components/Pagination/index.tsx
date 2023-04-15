12643f46daf38ea9a697656827edb102410de079
Marcelo Moraes <programadormarcello01@gmail.com>
2023-04-14 22:45:47 -0300

    deu bom

--- src/global/components/Pagination/index.tsx
+++ src/global/components/Pagination/index.tsx
@@ -1,55 +1,96 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import * as M from "@mui/material";
 import * as S from "./styles";
 import { INumberPages } from "./types";
+import { ArrowBack, ArrowForward } from '@mui/icons-material';
 
 export function PaginationComponent({
   pages,
   setCurrentPage,
   currentPage,
 }: INumberPages) {
-  const [localPage, setLocalPage] = useState<any>(currentPage);
+
+  const [first, setFirst] = useState<any>()
+
+  const MAX_ITEMS = 5
+  const MAX_LEFT = (MAX_ITEMS - 1) / 2
+  const totalPage = Math.ceil(pages)
+
+  useEffect(() => {
+    if (currentPage === totalPage) {
+      if (totalPage === 4) {
+        setFirst(currentPage - 3)
+      } else if (totalPage === 3) {
+        setFirst(currentPage - 2)
+      } else if (totalPage === 2) {
+        setFirst(currentPage - 1)
+      } else if (totalPage === 1) {
+        setFirst(Math.max(currentPage - MAX_LEFT, 1))
+      } else {
+        setFirst(currentPage - 4)
+      }
+    } else if (currentPage === totalPage - 1) {
+      if (totalPage - 1 === 3) {
+        setFirst(currentPage - 2)
+      } else if (totalPage - 1 === 2) {
+        setFirst(currentPage - 1)
+      } else if (totalPage - 1 === 1) {
+        setFirst(Math.max(currentPage - MAX_LEFT, 1))
+      } else {
+        setFirst(currentPage - 3)
+      }
+    } else {
+      setFirst(Math.max(currentPage - MAX_LEFT, 1))
+    }
+  }, [currentPage])
 
   const handlePrevPage = () => {
-    const newPage = Math.max(localPage - 1, 1);
-    setLocalPage(newPage);
+    const newPage = Math.max(currentPage - 1);
     setCurrentPage(newPage);
   };
 
   const handleNextPage = () => {
-    const newPage = Math.min(localPage + 1, pages);
-    setLocalPage(newPage);
+    const newPage = Math.min(currentPage + 1, pages);
     setCurrentPage(newPage);
   };
 
   return (
     <S.Pagination>
-      <button disabled={localPage === 1} onClick={handlePrevPage}>
-        Voltar
-      </button>
 
-      {Array.from(Array(pages), (_value, index) => {
-        const pageNumber = index + 1;
+      {currentPage > 0 && (     
+        <S.ContainerButton>      
+        <M.Button  
+        sx={{color: '#FFFFFF', marginRight: '10px'}} 
+        onClick={handlePrevPage}
+         disabled={currentPage === 0}
+        startIcon={<ArrowBack />}
+        />
+      </S.ContainerButton> 
+      )}
 
-        const handleClick = () => {
-          setLocalPage(pageNumber);
-          setCurrentPage(pageNumber);
-        };
+      {Array.from({ length: Math.min(MAX_ITEMS, totalPage) })
+        .map((_item, index) => index + first)
+        .map((page) => {
+          return (
+            <S.Button
+              key={page.id}
+              onClick={() => setCurrentPage(page)}
+              disabled={page === currentPage}
+            >
+              {page}
+            </S.Button>
+          );
+        })}
 
-        return (
-          <S.Button
-           key={index}
-           index={index}
-           value={index}
-           currentPage={currentPage}
-             onClick={handleClick}>
-            {pageNumber}
-          </S.Button>
-        );
-      })}
-
-      <button disabled={localPage === pages} onClick={handleNextPage}>
-        Avançar
-      </button>
+      {currentPage < pages && (        
+         <S.ContainerButton> 
+         <M.Button 
+         sx={{color: '#FFFFFF'}} 
+            onClick={handleNextPage}
+            disabled={currentPage === pages}
+            startIcon={<ArrowForward />}/>       
+        </S.ContainerButton>
+      )}
     </S.Pagination>
   );
 }
--- src/global/components/Pagination/styles.ts
+++ src/global/components/Pagination/styles.ts
@@ -1,23 +1,42 @@
-import styled from "styled-components"
-import { IStyledProps } from "./types"
+import styled from "styled-components";
+import { Box } from "@mui/material";
 
-export const Pagination = styled.ul`
-    height: 5rem;
-    display: flex;
-    @media (max-width: 768px) {
-        height: 10rem;
-    }
-`
+export const Pagination = styled(Box)`
+  display: flex;
+  justify-content: flex-end;
+  margin: 20px 95px 20px 0;
+  height: 5rem;
+  display: flex;
+  @media (max-width: 768px) {
+    height: 10rem;
+  }
+`;
 
-export const Button = styled.button<IStyledProps>`
-    height: 2rem;
-    width: 2rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: ${(props) => props.index === props.currentPage ? 'var(--blue-darksky)' : 'var(--blue-darksky)'};
-    color: var(--light);
-    margin-right: 0.5rem;
-    border-radius: var(--border-r);
-    transition: background-color 0.2s; 
-`
+export const Button = styled.button`
+  height: 2rem;
+  width: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--blue-darksky);
+  color: var(--light);
+  margin-right: 0.5rem;
+  border-radius: var(--border-r);
+  transition: background-color 0.2s;
+  cursor: pointer;
+`;
+export const ContainerButton = styled(Box)`
+  display: flex;
+  align-items: center;
+  height: 2rem;
+  width: 2rem;
+  border-radius: 10px;
+  background-color: var(--blue-darksky);
+  color: var(--light);
+  border-radius: 10px;
+  margin: 0 10px;
+
+  svg {
+    margin-right: 18px;
+  }
+`;
